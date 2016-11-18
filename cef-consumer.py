7dd02c5546aa34d1997521b129f58593295869d6
andyrat33 <andrew.ratcliffe@nswcsystems.co.uk>
2016-11-18 20:51:33 +0000

    First attempt work in progress

---
+++ cef-consumer.py
@@ -0,0 +1,61 @@
+
+from kafka import KafkaConsumer
+from cef_keys import cef_keys
+import re
+import sys
+import json
+
+print_keys = set()
+consumer = KafkaConsumer('arcsight',
+                         group_id='cef_consumer_group',
+                         bootstrap_servers=['kafka1:9092'])
+
+tokenlist = "|".join(cef_keys) 
+regex = re.compile('('+tokenlist+')=(.*?)\s(?:'+tokenlist+'|$)')
+
+
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    #print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          #message.offset, message.key,
+                                          #message.value))
+    parsed = {}
+    tokens = re.split(r'(?<!\\)\|', str(message.value, 'utf-8'))
+    Extension = ''
+    if len(tokens) == 8:
+        Extension = tokens[7] 
+    if len(tokens) > 8:
+        sys.stderr.write("Parsing error\n")
+        sys.exit(1)
+    parsed['CEFVersion'] = tokens[0].split('CEF:')[1]
+    parsed['DeviceVendor'] = tokens[1]
+    parsed['DeviceProduct'] = tokens[2]
+    parsed['DeviceVersion'] = tokens[3]
+    parsed['SignatureID'] = tokens[4]
+    parsed['Name'] = tokens[5]
+    parsed['Severity'] = tokens[6]
+
+    continue_parsing = False
+    if len(Extension) > 0:
+        continue_parsing = True
+    while continue_parsing:
+        m = re.search(regex, Extension)
+        try:
+            k,v = m.groups() 
+            parsed[k] = v
+            Extension = Extension.replace(k+'='+v, '').lstrip()
+        except AttributeError:
+            continue_parsing = False
+
+    o = {}
+    if len(print_keys) > 0:
+        for p in print_keys:
+            o[p] = parsed[p]
+    else:
+        o = parsed
+    print(json.dumps(o))    
+    
+    #for cef in str(message.value, 'utf-8').split('|'):
+        #print(" Value {!s}".format(cef))
+        
