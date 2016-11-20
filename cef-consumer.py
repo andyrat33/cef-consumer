7e0e11983b1dd13cd45e70eb07c41d70cc00e0a0
andyrat33 <andrew.ratcliffe@nswcsystems.co.uk>
2016-11-20 21:19:23 +0000

    removed cef_keys.py as no longer used

--- cef-consumer.py
+++ cef-consumer.py
@@ -1,6 +1,6 @@
 
 from kafka import KafkaConsumer
-from cef_keys import cef_keys
+
 import re
 import sys
 import json
@@ -17,42 +17,38 @@ cefRegexHeader = re.compile(r'(?<!\\)(\S+?)\|')
 cefRegexExtensions = re.compile(r'(\S+)(?<!\\)=')
 
 for message in consumer:
-    # message value and key are raw bytes -- decode if necessary!
-    # e.g., for unicode: `message.value.decode('utf-8')`
-    #print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          #message.offset, message.key,
-                                          #message.value))
-    print(str(message.value, 'utf-8'))
+    
+    #print(str(message.value, 'utf-8'))
     parsed = {}
     
     
-    #tokens = re.split(r'(?<!\\)\|', str(message.value, 'utf-8'))
+    
     counter = 0
     cefExtension = 0
     for matchHeader in re.finditer(cefRegexHeader, str(message.value, 'utf-8')):
         if counter == 0:
-            parsed['CEFVersion'] = matchHeader.group(1).split('CEF:')[1]
+            parsed['version'] = matchHeader.group(1).split('CEF:')[1]
         elif counter == 1:
-            parsed['DeviceVendor'] = matchHeader.group(1)
+            parsed['deviceVendor'] = matchHeader.group(1)
         elif counter == 2:
-            parsed['DeviceProduct'] = matchHeader.group(1)
+            parsed['deviceProduct'] = matchHeader.group(1)
         elif counter == 3:
-            parsed['DeviceVersion'] = matchHeader.group(1)
+            parsed['deviceVersion'] = matchHeader.group(1)
         elif counter == 4:
-            parsed['DeviceEventClassId'] = matchHeader.group(1)
+            parsed['deviceEventClassId'] = matchHeader.group(1)
         elif counter ==  5:
-            parsed['Name'] =  matchHeader.group(1)
+            parsed['name'] =  matchHeader.group(1)
         elif counter == 6:
-            parsed['Severity'] =  matchHeader.group(1)
+            parsed['severity'] =  matchHeader.group(1)
         counter += 1
     cefExtension = matchHeader.end()
     
     
     
-    #print(parsed)
+    # Remainder of message contains extension key, values
     Extension = str(message.value, 'utf-8')[cefExtension:].lstrip()
     #print(extension)
-    # use redis to cache resuls of token set creation 
+    # use the token list from the Extension as the method of getting data up to but not including the next key. 
     tokenlist = "|".join(set(re.findall(cefRegexExtensions, Extension)))
     extensionKeys = re.compile('('+tokenlist+')=(.*?)\s(?:'+tokenlist+'|$)')
     
@@ -75,6 +71,5 @@ for message in consumer:
         o = parsed
     print(json.dumps(o))    
     
-    #for cef in str(message.value, 'utf-8').split('|'):
-        #print(" Value {!s}".format(cef))
+    
         
