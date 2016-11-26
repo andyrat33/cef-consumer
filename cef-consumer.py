db01132cd7f90a77937997353c1927d9eea8bb49
andyrat33 <andrew.ratcliffe@nswcsystems.co.uk>
2016-11-26 21:16:54 +0000

    updated connection parameters and moved the config out of the call connect to kafka and es

--- cef-consumer.py
+++ cef-consumer.py
@@ -1,30 +1,32 @@
-from elasticsearch import Elasticsearch
-from kafka import KafkaConsumer
-
+#cef-consumer.py
 import re
 import sys
 import json
+from elasticsearch import Elasticsearch
+from kafka import KafkaConsumer
+
+if sys.version_info[0] < 3:
+    raise "Must be using Python 3"
+
+# kafka settings
+topic = 'arcsight'
+kafka = ['kafka2:9092']
+# elasticsearch
+elasticsearch = {'host': 'es2016', 'port': 9200}
 
 print_keys = set()
-consumer = KafkaConsumer('arcsight',
-                         group_id='cef_consumer_group',
-                         bootstrap_servers=['kafka1:9092'])
 
-
-
-from elasticsearch import Elasticsearch
-es = Elasticsearch([{'host': 'es2016', 'port': 9200}])
+consumer = KafkaConsumer(topic, group_id='cef_consumer_group',bootstrap_servers=kafka)
+es = Elasticsearch([elasticsearch])
 
 cefRegexHeader = re.compile(r'(?<!\\)(\S+?)\|')
 cefRegexExtensions = re.compile(r'(\S+)(?<!\\)=')
 i=0
 for message in consumer:
     i+=1
+
     #print(str(message.value, 'utf-8'))
     parsed = {}
-    
-    
-    
     counter = 0
     cefExtension = 0
     for matchHeader in re.finditer(cefRegexHeader, str(message.value, 'utf-8')):
@@ -44,22 +46,20 @@ for message in consumer:
             parsed['severity'] =  matchHeader.group(1)
         counter += 1
     cefExtension = matchHeader.end()
-    
-    
-    
+
     # Remainder of message contains extension key, values
     Extension = str(message.value, 'utf-8')[cefExtension:].lstrip()
-    #print(extension)
-    # use the token list from the Extension as the method of getting data up to but not including the next key. 
+    #print(Extension)
+    # use the token list from the Extension as the method of getting data up to but not including the next key.
     tokenlist = "|".join(set(re.findall(cefRegexExtensions, Extension)))
     extensionKeys = re.compile('('+tokenlist+')=(.*?)\s(?:'+tokenlist+'|$)')
-    
-    
+
+
     continue_parsing = True
     while continue_parsing:
         m = re.search(extensionKeys, Extension)
         try:
-            k,v = m.groups() 
+            k,v = m.groups()
             parsed[k] = v
             Extension = Extension.replace(k+'='+v, '').lstrip()
         except AttributeError:
@@ -72,6 +72,10 @@ for message in consumer:
     else:
         o = parsed
     print(json.dumps(o))    
-    
-    es.index(index='arcsight', doc_type='cef', id=i, body=json.loads(json.dumps(o)))
-        
+    #print("{eventId}".format(**parsed))
+
+    es.index(index='arcsight', doc_type='cef', id=parsed['eventId'], body=json.loads(json.dumps(o)))
+
+
+
+
