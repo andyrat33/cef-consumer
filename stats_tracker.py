9ccc08d1e5fe39f27903ba5e0fb74491de7b624e
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2017-01-03 23:01:35 +0000

    created timed thread for stats to send to redis

--- stats_tracker.py
+++ stats_tracker.py
@@ -1,6 +1,6 @@
 import redis
 import threading
-import datetime
+
 
 config = {
     'host': 'docker2',
@@ -11,22 +11,17 @@ config = {
 r = redis.StrictRedis(**config)
 counter = r.get(name="count")
 print("Current count : {}".format(int(counter)))
-r.hmset("test", dict(age=31, size=21))
-def storeStats():
-    threading.Timer(5.0, storeStats).start()
-    
 
-#storeStats()
-import threading
 
-def do_every (interval, worker_func, iterations = 0):
+
+def do_every(interval, worker_func, iterations = 0):
     if iterations != 1:
         threading.Timer (
             interval,
             do_every, [interval, worker_func, 0 if iterations == 0 else iterations-1]
             ).start ()
 
-    worker_func ()
+    worker_func()
 
 def print_hw ():
     print("hello world")
@@ -36,7 +31,7 @@ def print_so ():
 
 
 # call print_so every second, 5 times total
-do_every(1, print_so, 5)
+# do_every(1, print_so, 5)
 
 # call print_hw two times per second, forever
-do_every(25, print_hw)
+# do_every(25, print_hw)
